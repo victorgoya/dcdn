1c27e2308f008a21aec7d0ff7ac580fa6f178289
Victor Goya <goya.victor@gmail.com>
2017-11-02 00:14:26 +0100

    fix some mistake

--- app/controllers/api/contents_controller.rb
+++ app/controllers/api/contents_controller.rb
@@ -2,7 +2,13 @@ class Api::ContentsController < ApiController
   before_action :authenticate_user, only: [:create]
 
   expose :contents, -> { Content.all }
-  expose :content, fetch: -> { Content.find_by!(info_hash: params[:id] + "a") }
+  expose :content, fetch: -> {
+    if params[:id].present?
+      Content.find_by!(info_hash: params[:id])
+    else
+      Content.new(content_params)
+    end
+  }
 
   def index
     render json: contents
